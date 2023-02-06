1a2ad10ee692576b35f9c685f5f48ef507816c8b
Nurgissa Itkarinov <nurgissa.22@gmail.com>
2023-02-06 21:44:57 +0600

    day 1

--- twitter-clone/src/components/Home/TweetsList/index.js
+++ twitter-clone/src/components/Home/TweetsList/index.js
@@ -1,7 +1,50 @@
+import { KZ_IMG_PATH, N_IMG_PATH } from "../images/images"
+import Tweet from "./tweet"
+
 export default function TweetsList () {
+    const tweets = [
+        {
+            authorName: 'Free KZ Today',
+            authorUserName: '@kz',
+            img: KZ_IMG_PATH,
+            content: 'Random News',
+            retweets: 1000,
+            replies: 200,
+            likes: 500,
+            comments: 30
+        },
+        {
+            authorName: 'nFactorial',
+            authorUserName: '@nfactorial',
+            img: N_IMG_PATH,
+            content: 'Data analytics course starts today!',
+            retweets: 766,
+            replies: 199,
+            likes: 467,
+            comments: 23
+        },
+        {
+            authorName: 'Free KZ Today',
+            authorUserName: '@kz',
+            img: KZ_IMG_PATH,
+            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            retweets: 1000,
+            replies: 200,
+            likes: 500,
+            comments: 30
+        },
+        {
+            authorName: 'nFactorial',
+            authorUserName: '@nfactorial',
+            img: N_IMG_PATH,
+            content: 'Data analytics course starts today!',
+            retweets: 766,
+            replies: 199,
+            likes: 467,
+            comments: 23
+        },
+    ]
     return (
-        <div>
-            
-        </div>
+        tweets.map((tweet, index) => <Tweet {...tweet} key={index}/>)
     )
 }
--- twitter-clone/src/components/SideMenu/menuItem.js
+++ twitter-clone/src/components/SideMenu/menuItem.js
@@ -1,7 +1,7 @@
 export default function MenuItem( {item} ) {
     return (
         <div style={{display: 'flex', alignItems: 'center', margin: '50px 5px'}}>
-            <div style={{width: 40, height: 40}}>
+            <div style={{width: 30, height: 30}}>
                 {item.icon}
             </div>
             <p style={{fontSize: 18, margin: "0px 15px", }} >{item.name}</p>
