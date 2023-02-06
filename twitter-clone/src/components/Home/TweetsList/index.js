9c765c21f6e6d4a5b4279670c0683486a6c05aff
Nurgissa Itkarinov <nurgissa.22@gmail.com>
2023-02-06 18:11:41 +0600

    day 1

--- twitter-clone/src/components/Home/TweetsList/index.js
+++ twitter-clone/src/components/Home/TweetsList/index.js
@@ -1,5 +1,7 @@
 export default function TweetsList () {
     return (
-        <p>Tweets List</p>
+        <div>
+            
+        </div>
     )
 }
--- twitter-clone/src/components/Home/index.js
+++ twitter-clone/src/components/Home/index.js
@@ -3,8 +3,8 @@ import NewTweet from './NewTweet'
 import TweetsList from './TweetsList'
 export default function Home () {
     return (
-        <div className="home" style={{marginTop: 50, marginLeft: 10}}>
-            <p style={{fontWeight: 600}}>Home</p>
+        <div className="home">
+            <p style={{fontWeight: 600, paddingLeft: 10}}>Home</p>
             <NewTweet/>
             <TweetsList/>
         </div>
